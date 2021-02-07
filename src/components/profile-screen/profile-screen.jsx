41decc2489038c79c009a56381fbe4dbc3a95a16
webproject67 <webproject67@gmail.com>
2021-02-07 13:28:02 +0300

    Добавил валидатор в Мой профиль

--- src/components/profile-screen/profile-screen.jsx
+++ src/components/profile-screen/profile-screen.jsx
@@ -3,6 +3,30 @@ import PropTypes from "prop-types";
 
 const ProfileScreen = (props) => {
   const {onButtonClickTask, onButtonClickStatistics} = props;
+
+  const onSubmitClick = (evt) => {
+    evt.preventDefault();
+
+    const name = document.querySelector("[name=name]");
+    const surname = document.querySelector("[name=surname]");
+    const textError = document.querySelectorAll(".main__text-error");
+    const submit = document.querySelector(".main__submit");
+    
+    if (!name.value) {
+      textError[0].classList.add("main__text-error--opacity");
+      name.classList.add("main__name--border");
+    } else if (!surname.value) {
+      textError[0].classList.remove("main__text-error--opacity");
+      name.classList.remove("main__name--border");
+      textError[1].classList.add("main__text-error--opacity");
+      surname.classList.add("main__name--border");
+    } else {
+      textError[1].classList.remove("main__text-error--opacity");
+      surname.classList.remove("main__name--border");
+      submit.classList.add("main__submit--width");
+      submit.value = 'Спасибо! Форма отправлена успешно!'
+    }
+  };
   
   const onButtonClick = () => {
     const button = document.querySelector(".main__arrow");
@@ -38,13 +62,15 @@ const ProfileScreen = (props) => {
       <section className="main__profile">
         <button onClick={onButtonClick} className="main__arrow" type="button" aria-label="toggle"></button>
         <h2 className="main__text">Мой профиль</h2>
-        <form className="main__form">
+        <form onSubmit={onSubmitClick} className="main__form">
           <div className="main__wrapper">
             <div className="main__data">
               <label htmlFor="name">Имя</label>
-              <input className="main__name" type="text" name="name" id="name" required/>
+              <input className="main__name" type="text" name="name" id="name"/>
+              <p className="main__text-error">Пожалуйста заполните поле</p>
               <label htmlFor="surname">Фамилия</label>
-              <input className="main__surname" type="text" name="surname" id="surname" required/>
+              <input className="main__surname" type="text" name="surname" id="surname"/>
+              <p className="main__text-error">Пожалуйста заполните поле</p>
             </div>
             <div className="main__change">
               <label htmlFor="password">Сменить пароль</label>
